49e6dc074c8a8bd67231eddce179e2abac744a6f
migasrias2 <migasparavinted@gmail.com>
2025-01-15 12:09:47 +0000

    update logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ interface LogoProps {
 export function Logo({ className = "w-10 h-10" }: LogoProps) {
   return (
     <img
-      src="Petnessa.png"
+      src="./src/Petnessa.png"
       alt="Petnessa"
       className={className}
     />
